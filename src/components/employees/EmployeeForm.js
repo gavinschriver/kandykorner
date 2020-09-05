93401c8ab56a263fc86d9b1432dee8a6612204f2
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-05 15:22:47 -0500

    feelng validated

--- src/components/employees/EmployeeForm.js
+++ src/components/employees/EmployeeForm.js
@@ -1,30 +1,66 @@
-import React, { useContext, useEffect } from "react";
-import { LocationContext } from "../locations/LocationProvider"
+import React, { useContext, useEffect, useRef } from "react";
+import { LocationContext } from "../locations/LocationProvider";
 
 export const EmployeeForm = () => {
+  const { locations } = useContext(LocationContext);
 
-const { locations } = useContext(LocationContext)
+  const hireName = useRef(null);
+  const hireLoc = useRef(null);
+  const hireIsManager = useRef("0");
+  const hireIsFullTime = useRef("0");
+  const hirePay = useRef(null);
+
+  const addHire = () => {
+    if (
+      hireIsManager.current.value !== "coocoobananapants" &&
+      hireIsFullTime.current.value !== "coocoobananapants"
+    ) {
+      const newHire = {
+        name: hireName.current.value,
+        locationId: parseInt(hireLoc.current.value),
+        manager: JSON.parse(hireIsManager.current.value),
+        fulltime: JSON.parse(hireIsFullTime.current.value),
+        payrate: parseFloat(hirePay.current.value),
+      };
+
+      if (
+        hireName.current.value !== "" &&
+        hireLoc.current.value &&
+        hirePay.current.value
+      ) {
+        console.log(newHire);
+      }
+    } else alert("please fill out all forms to my utter satisfaction");
+  };
 
   return (
     <form>
-          <input type="text" placeholder="enter employee name" />
-          <select>
-              <option value="">Please choose a location</option>
-              {
-                  locations.map(l => <option>{l.address}</option>)
-              }
-          </select>
-          <select>
-              <option>Manager?</option>
-              <option>Yes</option>
-              <option>No</option>
-          </select>
-          <select>
-              <option>Full or Part Time</option>
-              <option>Full-Time</option>
-              <option>Part-Time</option>
-          </select>
-          <input type="number" placeholder="enter hourly pay"></input>
+      <input type="text" placeholder="enter employee name" ref={hireName} />
+      <select ref={hireLoc}>
+        <option value="">Please choose a location</option>
+        {locations.map((l) => (
+          <option value={l.id}>{l.address}</option>
+        ))}
+      </select>
+      <select ref={hireIsManager}>
+        <option value="coocoobananapants">Manager?</option>
+        <option value={true}>Yes</option>
+        <option value={false}>No</option>
+      </select>
+      <select ref={hireIsFullTime}>
+        <option value="coocoobananapants">Full or Part Time</option>
+        <option value={true}>Full-Time</option>
+        <option value={false}>Part-Time</option>
+      </select>
+      <input type="number" placeholder="enter hourly pay" ref={hirePay}></input>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          addHire();
+        }}
+      >
+        YA HIRED
+      </button>
     </form>
   );
 };
